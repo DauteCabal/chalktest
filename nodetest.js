9db18b19a6f72bf135ef3ad7f67c1ac602e3a56a
Daute <daute@daute.dev>
2020-09-30 15:44:02 +0200

    First upload of chalk test

---
+++ nodetest.js
@@ -0,0 +1,3 @@
+const chalk = require('chalk');
+ 
+console.log(`My colleagues are ${chalk.blue('Rado')}, ${chalk.red('Martin')}, ${chalk.yellow('Matt')}, ${chalk.grey('Kasper')}, and ${chalk.green('Joeway')}`)
